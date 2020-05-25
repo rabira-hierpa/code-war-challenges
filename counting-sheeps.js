46c7b37d426d8729598a703ba5c4fb0f19ce4959
Rabra <rabira.hierpa@gmail.com>
2020-05-25 08:51:19 +0300

    Passed the 3rd challenge

---
+++ counting-sheeps.js
@@ -0,0 +1,58 @@
+// Counting Sheeps - Challenge #3
+
+OfficalLink =
+	'https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript';
+
+// My first approach to the problem
+function countSheeps(arrayOfSheep) {
+	// --> Refactored appraoch
+	return arrayOfSheep.reduce((total, precence) => {
+		return precence ? (total += 1) : total;
+	}, 0);
+	// --> First proach
+	var total = 0;
+	arrayOfSheep.map((ele) => {
+		ele ? (total += 1) : null;
+	});
+	return total;
+}
+
+// --> Tests
+var array1 = [
+	true,
+	true,
+	true,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	true,
+	false,
+	true,
+	false,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	false,
+	true,
+	true,
+];
+
+var array2 = [true, true, false, true];
+
+console.log(countSheeps(array1));
+
+// --> Best Practice + Clever solution
+function countSheeps(arrayOfSheeps) {
+	// The filter method acceptes the Boolean function as a test function and test each element in the array
+	return arrayOfSheeps.filter(Boolean).length;
+}
+
+// Test.expect(countSheeps(array1) == 17, 'There are 17 sheeps in total');
