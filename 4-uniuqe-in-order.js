6f74e135a00bf84cb470ecdf076f382a56b75587
Rabra <rabira.hierpa@gmail.com>
2020-05-31 08:30:01 +0300

    Passed the 4th challenge

--- 4-uniuqe-in-order.js
+++ 4-uniuqe-in-order.js
@@ -4,17 +4,23 @@ OfficalLink =
 	'https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript';
 
 // My first approach to the problem
+// --> NO StackOverflow !!!
+// --> Docs allowed (MSDN and w3schools)
 var uniqueInOrder = function (iterable) {
-	//your code here - remember iterable can be a string or an array
+	return Number.isNaN(iterable)
+		? iterable.split('').filter((value, index, array) => {
+				console.log(value);
+				return value !== array[index + 1];
+		  })
+		: [...iterable].filter((value, index, array) => {
+				return value !== array[index + 1];
+		  });
 };
-
 // --> Tests
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+console.log(uniqueInOrder('AAAABBBCCDAABBB'));
 
-Test.assertSimilar(uniqueInOrder('AAAABBBCCDAABBB'), [
-	'A',
-	'B',
-	'C',
-	'D',
-	'A',
-	'B',
-]);
+// --> Best practice + Clever solution
+var uniqueInOrder = function (iterable) {
+	return [...iterable].filter((a, i) => a !== iterable[i - 1]);
+};
