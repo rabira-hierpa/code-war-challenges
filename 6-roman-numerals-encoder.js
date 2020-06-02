3cb6a00a64c1fa97880b074f5d2652389819634b
Rabra <rabira.hierpa@gmail.com>
2020-06-02 08:04:16 +0300

    Added the 6th Challenge - Roman Numerals Encoder

---
+++ 6-roman-numerals-encoder.js
@@ -0,0 +1,55 @@
+// Roman numerals encoder  - Challenge #4
+
+OfficalLink =
+	'https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript';
+
+// My first approach to the problem
+// --> NO StackOverflow !!!
+// --> Docs allowed (MSDN and w3schools)
+function solution(number) {
+	// convert the number to a roman numeral
+}
+
+// --> Tests
+solution(1);
+solution(2);
+solution(3);
+solution(4);
+solution(5);
+solution(9);
+solution(10);
+solution(11);
+solution(19);
+solution(22);
+solution(15);
+
+solution(1000);
+solution(1001);
+solution(1990);
+solution(2007);
+solution(2008);
+
+// --> Codware test using mocah
+// describe('solution', () => {
+// 	it('should handle small numbers', () => {
+// 		Test.assertEquals(solution(2), 'II', '2 should, "II"');
+// 		Test.assertEquals(solution(1), 'I', '1 should, "I"');
+// 		Test.assertEquals(solution(3), 'III', '3 should, "III"');
+// 		Test.assertEquals(solution(4), 'IV', '4 should, "IV"');
+// 		Test.assertEquals(solution(5), 'V', '5 should, "V"');
+// 		Test.assertEquals(solution(9), 'IX', '9 should, "IX"');
+// 		Test.assertEquals(solution(10), 'X', '10 should, "X"');
+// 		Test.assertEquals(solution(11), 'XI');
+// 		Test.assertEquals(solution(19), 'XIX');
+// 		Test.assertEquals(solution(22), 'XXII');
+// 		Test.assertEquals(solution(15), 'XV');
+// 	});
+
+// 	it('should handle large numbers', () => {
+// 		Test.assertEquals(solution(1000), 'M', '1000 should, "M"');
+// 		Test.assertEquals(solution(1001), 'MI', '1001 should, "MI"');
+// 		Test.assertEquals(solution(1990), 'MCMXC', '1990 should, "MCMXC"');
+// 		Test.assertEquals(solution(2007), 'MMVII', '2007 should, "MMVII"');
+// 		Test.assertEquals(solution(2008), 'MMVIII', '2008 should, "MMVIII"');
+// 	});
+// });
