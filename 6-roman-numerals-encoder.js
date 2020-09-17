a8a211c36ed8ce8bd2d53ce3350d6ba079025384
Rabra <rabira.hierpa@gmail.com>
2020-09-17 08:20:08 +0300

    Added best soultion by codewars

--- 6-roman-numerals-encoder.js
+++ 6-roman-numerals-encoder.js
@@ -67,6 +67,39 @@ function solution(number) {
 	return convertedRoman.join('');
 }
 
+// Codewars best solution
+
+function convertToRoman(number) {
+	// convert the number to a roman numeral
+	var roman = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1,
+	};
+
+	var ans = '';
+	while (number > 0) {
+		for (a in roman) {
+			if (roman[a] <= number) {
+				ans += a;
+				number -= roman[a];
+				break;
+			}
+		}
+	}
+	return ans;
+}
+
 // --> Tests
 console.log(solution(1));
 console.log(solution(2));
