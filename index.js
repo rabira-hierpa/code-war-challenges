dd9376ede0bb2a363d9211361ec223846517c1c1
Rabra <rabira.hierpa@gmail.com>
2020-09-19 12:16:32 +0300

    Added the 10th challenge

--- index.js
+++ index.js
@@ -1,69 +1,33 @@
-// List Filtering - Challenge #8
+// First non-repeating character - Challenge #10
 
 OfficalLink =
-	'https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/javascript';
+	'https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript';
 
 // My first approach to the problem
 // --> NO StackOverflow !!!
 // --> NO YouTube !!!
 // --> Docs allowed (MSDN and w3schools)
 
-function solution(str) {
-	let charLength = str.length;
-	let array = [];
-	if (charLength % 2 === 0) {
-		let idx = 0;
-		str.split('').forEach((element) => {
-			str[idx] ? array.push(str.substr(idx, 2)) : null;
-			idx += 2;
-		});
-		return array;
-	} else {
-		let idx = 0;
-		str.split('').forEach((element, index) => {
-			str[idx] ? array.push(str.substr(idx, 2)) : null;
-			idx += 2;
-		});
-		array.pop();
-		var last = charLength - 1;
-		array.push(str[last] + '_');
-		return array;
-	}
+function firstNonRepeatingLetter(s) {
+  // Add your code here
 }
 
 // Codewars best soution
 function spliString1(str) {
-	var i = 0;
-	var result = new Array();
-	if (str.length % 2 !== 0) {
-		str = str + '_';
-	}
-	while (i < str.length) {
-		result.push(str[i] + str[i + 1]);
-		i += 2;
-	}
-	return result;
-}
 
-// ==> Using Regex
-function splitStringRegex(s) {
-	return (s + '_').match(/.{2}/g) || [];
-}
 // --> Tests
 
-console.log(solution('abcde'));
-console.log(solution('abcdef'));
-console.log(solution('abcdefg'));
-console.log(solution('abcdefghijk'));
+console.log(solution('stress'));
+console.log(solution('moomen'));
+console.log(solution('tata'));
+console.log(solution('cars'));
 console.log(solution(''));
 console.log(solution('a'));
 // --> Codware test using mocah
-// const { assert } = require('chai');
-
-// describe('Split Strings', () => {
-// 	it('Basic tests', () => {
-// 		assert.deepEqual(solution('abcdef'), ['ab', 'cd', 'ef']);
-// 		assert.deepEqual(solution('abcdefg'), ['ab', 'cd', 'ef', 'g_']);
-// 		assert.deepEqual(solution(''), []);
-// 	});
+// Test.describe('Simple Tests', function() {
+//   it('should handle simple tests', function() {
+//     Test.assertEquals(firstNonRepeatingLetter('a'), 'a');
+//     Test.assertEquals(firstNonRepeatingLetter('stress'), 't');
+//     Test.assertEquals(firstNonRepeatingLetter('moonmen'), 'e');
+//   });
 // });
