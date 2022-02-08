1101e108af5bef985dbf72d611d5122fb9ec4364
Rabra Hierpa <rabira.hierpa@gmail.com>
2022-02-08 09:31:47 +0300

    refactored if conditions to ternary operators typo fix

--- javascript-challenges/12-findMaxConsecutiveOnes.ts
+++ javascript-challenges/12-findMaxConsecutiveOnes.ts
@@ -2,7 +2,7 @@
  * Function that returns max consecutive ones
  * @abstract Given a binary array nums, return the maximum number of consecutive 1's in the array.
  * @copyright Rabra H.
- * @param nums num arra
+ * @param nums number array
  * @returns max consecutive ones
  */
 function findMaxConsecutiveOnes(nums: number[]): number {
@@ -12,22 +12,19 @@ function findMaxConsecutiveOnes(nums: number[]): number {
   }
   if (nums.length <= Math.pow(10, 5)) {
     let current = 0;
-    for (let i = 0; i < nums.length; ++i) {
-      if (nums[i] === 1) {
-        current += 1;
-      } else {
-        current = 0;
-      }
+    for (let i = 0; i < nums.length; i++) {
+	  nums[i] === 1 ? current +=1 : current = 0
       max = current > max ? current : max;
     }
   }
   return max;
 }
 
-console.log(findMaxConsecutiveOnes([1, 1, 0, 0, 1, 1, 1, 0, 1]));
 console.log(findMaxConsecutiveOnes([1]));
 console.log(findMaxConsecutiveOnes([]));
+console.log(findMaxConsecutiveOnes([1,0,1,1,0]));
+console.log(findMaxConsecutiveOnes([1, 1, 0, 0, 1, 1, 1, 0, 1]));
 console.log(findMaxConsecutiveOnes([1, 0, 1, 0, 1, 1, 0, 0, 1]));
 console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1, 1, 0, 1]));
 console.log(findMaxConsecutiveOnes([1, 1, 0, 0, 1, 1, 1, 0, 1]));
-console.log(findMaxConsecutiveOnes([1, 1, 1, 0, 1, 1, 1, 0, 1]));
+console.log(findMaxConsecutiveOnes([1, 0, 1, 0, 1, 0, 1, 1, 1]));
