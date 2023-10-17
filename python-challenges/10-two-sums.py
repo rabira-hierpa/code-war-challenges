e2e1992d068a9f0c6e1a074bf516698808f9b87f
rabra <rabira.hierpa@gmail.com>
2023-10-17 10:04:35 +0300

    Refactor two_sum function and add optimized solution in 10-two-sums.py

--- python-challenges/10-two-sums.py
+++ python-challenges/10-two-sums.py
@@ -1,15 +1,20 @@
 # 10th python challenge -Two sum
 
+from typing import List
+
+
 OfficalLink = 'https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/python'
 
-# My first approach to the problem
-# --> NO StackOverflow !!!
-# --> NO YouTube !!!
-# --> Docs allowed(MSDN and w3schools)
+# * My first approach to the problem
+# ! --> NO StackOverflow !!!
+# ! --> NO YouTube !!!
+# * --> Docs allowed(MSDN and w3schools)
 
 
 def two_sum(numbers, target):
     for idx, val in enumerate(numbers):
+        if len(numbers) == 2 and numbers[idx] + numbers[idx+1] == target:
+            return [0, 1]
         if idx > 0 and idx < len(numbers) - 1:
             prev = numbers[idx - 1]
             next_ = numbers[idx + 1]
@@ -27,6 +32,22 @@ def two_sum2(nums, t):
         for j, y in enumerate(nums):
             if i != j and x + y == t:
                 return [i, j]
+
+# Leetcode solution
+# Time complexity: O(n)
+# Space complexity: O(n)
+# Approach - one-pass hash table - uses complement approach i.e instead of adding each number to the target
+# and checking if the complement exists in the array, we can check if the complement exists in the hashmap
+# and then return the indices else add it to the hashmap
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    hashmap = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap:
+            return [i, hashmap[complement]]
+        hashmap[nums[i]] = i
 
 
 # Test
